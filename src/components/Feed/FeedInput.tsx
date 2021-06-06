1f9d9d2b1393022e5bb3f49d4916feba46365d79
Christopher <onlygamernerd@gmail.com>
2021-06-06 17:44:34 +0200

    update FeedInput

--- src/components/Feed/FeedInput.tsx
+++ src/components/Feed/FeedInput.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const FeedInput: React.FC = () => {
+	const [message, setMessage] = useState('')
+
+	const handleSendMessage = () => {
+		console.log(message)
+	}
+
 	return (
 		<div className="mb-7">
 			<div>
 				<textarea placeholder="Hvordan har du det?" className="w-96 resize-none p-1 pb-7 rounded-sm outline-none"/>
 			</div>
-			<button className="bg-indigo-500 rounded-sm px-6 py-1 text-gray-100">Send</button>
+			<button
+				onChange={(e) => setMessage(e.currentTarget.value)}
+				className="bg-indigo-500 rounded-sm px-6 py-1 text-gray-100"
+				onClick={handleSendMessage}
+			>
+				Send
+			</button>
 		</div>
 	)
 }
